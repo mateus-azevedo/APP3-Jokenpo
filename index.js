ba8af3c8c1ddbd30b55103d8f552d97c5ece6ca6
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-28 16:47:36 -0300

    entendendo um pouco de props

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-/**
- * @format
- */
+import React, {Component} from 'react';
+import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+class MeuComponente extends Component {
+  render() {
+    return (
+      <View>
+        <Text>{this.props.propriedade1}</Text>
+        <Text>{this.props.xyz}</Text>
+        <Text>{this.props.p}</Text>
+      </View>
+    );
+  }
+}
 
-AppRegistry.registerComponent(appName, () => App);
+class app3 extends Component {
+  render() {
+    return <MeuComponente propriedade1='Banana' xyz='Abacaxi' p='Uva' />;
+  }
+}
+
+AppRegistry.registerComponent('app3', () => app3);
