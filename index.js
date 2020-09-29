64ed8f29759d5ca77b1bb298d277ac80ce4e1f23
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-28 23:26:20 -0300

    implementado a lógica do computador

--- index.js
+++ index.js
@@ -6,17 +6,32 @@ class app3 extends Component {
   constructor(props){
     super(props);
 
-    this.state = { escolhaUsuario : ''}
+    this.state = { escolhaUsuario : '', escolhaComputador : ''}
   }
 
   jokenpo(escolhaUsuario){
-    this.setState({ escolhaUsuario : escolhaUsuario });
+
+    var numAleatorio = Math.floor(Math.random() * 3);
+    
+    var escolhaComputador = '';
+    
+    switch(numAleatorio){
+      case 0: escolhaComputador = 'pedra'; break;
+      case 1: escolhaComputador = 'papel'; break;
+      case 2: escolhaComputador = 'tesoura'; break;
+    }
+    
+    this.setState({ 
+      escolhaUsuario : escolhaUsuario ,
+      escolhaComputador : escolhaComputador
+    });
+    
   }
   
   render(){
       return (
         <View>
-          <Text>Escolha do Computador</Text>
+          <Text>Escolha do Computador: {this.state.escolhaComputador}</Text>
           <Text>Escolha do Usuário: {this.state.escolhaUsuario}</Text>
           <Text>Resultado</Text>
           <Button title='pedra' onPress={ () => {this.jokenpo('pedra')} } />
