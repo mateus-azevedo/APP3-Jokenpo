3971a61d1ca9a61c1ab30a72f8a8ce3af1544a96
Mateus Santos <m.santos.azeve@gmail.com>
2020-09-28 23:40:03 -0300

    implementado a lógica do resultado do jogo

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ class app3 extends Component {
   constructor(props){
     super(props);
 
-    this.state = { escolhaUsuario : '', escolhaComputador : ''}
+    this.state = {
+      escolhaUsuario : '', 
+      escolhaComputador : '',
+      resultado: ''
+    }
   }
 
   jokenpo(escolhaUsuario){
@@ -21,9 +25,55 @@ class app3 extends Component {
       case 2: escolhaComputador = 'tesoura'; break;
     }
     
+    var resultado = '';
+
+    if(escolhaComputador == 'pedra'){
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Empate';
+      }
+
+      if(escolhaUsuario == 'papel'){
+        resultado = 'Usuário ganhou';
+      }
+
+      if(escolhaUsuario == 'tesoura'){
+        resultado = 'Computador ganhou'
+      }
+    }
+
+    if(escolhaComputador == 'papel'){
+      if(escolhaUsuario == 'papel'){
+        resultado = 'Empate';
+      }
+
+      if(escolhaUsuario == 'tesoura'){
+        resultado = 'Usuário ganhou';
+      }
+
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Computador ganhou'
+      }
+    }
+    
+    if(escolhaComputador == 'tesoura'){
+      if(escolhaUsuario == 'tesoura'){
+        resultado = 'Empate';
+      }
+
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Usuário ganhou';
+      }
+
+      if(escolhaUsuario == 'papel'){
+        resultado = 'Computador ganhou'
+      }
+    }
+    
+    
     this.setState({ 
       escolhaUsuario : escolhaUsuario ,
-      escolhaComputador : escolhaComputador
+      escolhaComputador : escolhaComputador,
+      resultado : resultado
     });
     
   }
@@ -33,7 +83,7 @@ class app3 extends Component {
         <View>
           <Text>Escolha do Computador: {this.state.escolhaComputador}</Text>
           <Text>Escolha do Usuário: {this.state.escolhaUsuario}</Text>
-          <Text>Resultado</Text>
+          <Text>Resultado: {this.state.resultado}</Text>
           <Button title='pedra' onPress={ () => {this.jokenpo('pedra')} } />
           <Button title='papel' onPress={ () => {this.jokenpo('papel')} } />
           <Button title='tesoura' onPress={ () => {this.jokenpo('tesoura')} } />
